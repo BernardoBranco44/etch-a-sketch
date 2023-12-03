4f214b32622cb218a67267925fa9f7bae5bbf724
BernardoBranco44 <bernardomspbranco@gmail.com>
2023-12-03 23:43:02 +0000

    added feature for +0.1 opacity for every hover until 1.0

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ const button = document.querySelector(".change-btn")
 button.addEventListener("click", () => changeGrid())
 
 function changeGrid() {
-    console.log("click")
     let gridSize = prompt("Number of squares per side", "Max of 100")
     if (gridSize < 101) {
         container.textContent = ""
@@ -12,8 +11,16 @@ function changeGrid() {
             const div = document.createElement("div");
             container.appendChild(div)
             div.style.width = `${100/gridSize}%`
+            div.style.opacity = 0
+            let op = 0
             div.addEventListener("mouseenter", () => {
-                div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+                op += 0.1
+                if (div.style.opacity == 0) {
+                    div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+                }
+                if (div.style.opacity < 1) {
+                    div.style.opacity = op
+                }
             })
             div.addEventListener("mouseleave", () => {
         
